ce3047c53772bf6ffd626c29bab0c1de4dc39125
ShushilShah <shahshushil3@gmail.com>
2024-01-28 12:50:50 +0545

    recommendation is made more precise

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import pickle
 book = pickle.load(open('book.pkl', 'rb'))
 app = Flask(__name__)
 
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(book['Genre'])
+
 
 @app.route("/")
 def Book_recommendation():
@@ -17,8 +20,24 @@ def Book_recommendation():
                            )
 
 
-@app.route("/recommend")
+@app.route("/recommend", methods=['GET', 'POST'])
 def recommend():
+    if request.method == 'POST':
+        user_input = request.form['book_title']
+
+        user_tfidf = tfidf_vectorizer.transform([user_input])
+
+        # Computing similarity
+        similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+        # Recommendations
+        # index = data[data['Title'] == title].index[0]
+        # score = list(enumerate(similarity[index]))
+        # score = sorted(score, key=lambda x: x[1], reverse=True)[1:4]
+        book_indices = similarity.argsort()[1:4]
+        recommended_book = book.iloc[book_indices][['Title', 'Genre']]
+        return render_template('recommend.html', user_input=user_input, recommended_book=recommended_book.to_dict(orient='records'))
+
     return render_template('recommend.html')
 
 
